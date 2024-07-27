6e08b1f1d84f56f23e0b8242656768747601caa1
hayfa.jundi <haifazaqwan@gmail.com>
2024-07-27 23:51:44 +0300

    api

--- API_Test/app.py
+++ API_Test/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_restful import Api, Resource
 import pickle
